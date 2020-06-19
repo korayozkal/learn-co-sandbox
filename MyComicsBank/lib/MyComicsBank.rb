f6503a80326cf04b3a2a0da2ac0dec025dc6c633
Koray Ozkal <koryozkal@gmail.com>
2020-06-19 21:44:17 +0000

    first commit

---
+++ MyComicsBank/lib/MyComicsBank.rb
@@ -0,0 +1,6 @@
+require "MyComicsBank/version"
+
+module MyComicsBank
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ MyComicsBank/lib/MyComicsBank/version.rb
@@ -0,0 +1,3 @@
+module MyComicsBank
+  VERSION = "0.1.0"
+end
